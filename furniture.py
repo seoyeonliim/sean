a19fa364f81c1249a135e25b6118a5ab97df9833
seoyeonliim <130126753+seoyeonliim@users.noreply.github.com>
2023-04-30 09:30:33 +0900

    웰니스 / 가구 인테리어 봇
    웰니스 데이터와 가구 인테리어 csv파일을 이용한, 챗봇 생성 데이터(python)

---
+++ furniture.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from streamlit_chat import message
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+import json
+
+@st.cache(allow_output_mutation=True)
+def cached_model():
+    model = SentenceTransformer('jhgan/ko-sroberta-multitask')
+    return model
+
+@st.cache(allow_output_mutation=True)
+def get_dataset():
+    df = pd.read_csv('furniture.csv')
+    df['embedding'] = df['embedding'].apply(json.loads)
+    return df
+
+model = cached_model()
+df = get_dataset()
+
+st.header('가구인테리어 챗봇')
+
+if 'generated' not in st.session_state:
+    st.session_state['generated']=[]
+    
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+    
+with st.form('form', clear_on_submit = True):
+    user_input = st.text_input('당신: ', '')
+    submitted = st.form_submit_button('전송')
